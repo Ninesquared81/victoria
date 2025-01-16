a96741587c61b67f3d41bb13fb4cbdf106d61a79
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2025-01-16 19:46:08 +0000

    Add ast

---
+++ bootstrap/rvic-c_ast.h
@@ -0,0 +1,51 @@
+#ifndef RVIC_C_AST
+#define RVIC_C_AST
+
+#include <stdint.h>  // int64_t
+
+enum ast_node_kind {
+    AST_KIND_EXPR,  // Expression.
+    AST_KIND_STMT,  // Statement.
+};
+
+enum ast_expr_kind {
+    AST_EXPR_INTEGER,  // Integer literal expression.
+    AST_EXPR_BINARY,   // Binary operation.
+};
+
+enum ast_stmt_kind {
+    AST_STMT_EXPR,  // Expression statement.
+};
+
+struct ast_expr {
+    enum ast_expr_kind kind;
+    union {
+        struct {
+            int64_t value;
+        } integer;
+        struct {
+            struct expr *lhs;
+            struct expr *rhs;
+            const char *op;
+        } binary;
+    };
+};
+
+struct ast_stmt {
+    enum ast_stmt_kind kind;
+    union {
+        struct {
+            struct ast_expr *expr;
+        } expr;
+    };
+};
+
+struct ast_node {
+    enum ast_node_kind kind;
+    union {
+        struct ast_expr *expr;
+        struct ast_stmt *stmt;
+    };
+};
+
+#endif
