46a39427bb3dd7db56475d6b1bbc97fb6c36bac1
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2025-01-16 23:14:04 +0000

    Add basic code generator for the current (very simple) grammar

---
+++ bootstrap/rvic-c_backend.c
@@ -0,0 +1,52 @@
+#include <asset.h>
+#include <inttypes.h>
+
+#include "rvic-c_backend.h"
+
+enum cgen_error rvic_c_generate_c_file(int n, const struct ast_node nodes[static const n], FILE *f) {
+    assert(n >= 0);
+    fprintf(f, "int main(void) {\n");
+    int indent_step = 4;  // Number of spaces to indent by for each indent/dedent.
+    int indent = indent_step;  // Current indenation level.
+    enum cgen_error error = CGEN_OK;
+    for (int i = 0; i < n; ++i) {
+        struct ast_node node = nodes[i];
+        switch (node.kind) {
+        case AST_EXPR:
+            return CGEN_UNEXPECTED_EXPR;
+        case AST_STMT:
+            error = rvic_c_generate_c_stmt(node.stmt, indent, indent_step);
+            break;
+        }
+        if (error) return error;
+    }
+    fprintf(f, "    return 0;\n}\n");
+    return CGEN_OK;
+}
+
+enum cgen_error rvic_c_generate_c_stmt(struct ast_stmt stmt, int indent, int indent_step, FILE *f) {
+    fprintf(f, "%*s", indent, "");  // Prepend indentation.
+    enum cgen_error error = CGEN_OK;
+    switch (stmt.kind) {
+    case AST_STMT_EXPR:
+        error = rvic_c_generate_c_expr(stmt.expr);
+        fprintf(f, ";\n");
+        break;
+    }
+    return error;
+}
+
+enum cgen_error rvic_c_generate_c_expr(struct ast_expr expr, FILE *f) {
+    enum cgen_error error = CGEN_OK;
+    switch (expr.kind) {
+    case AST_EXPR_INTEGER:
+        if (!fprintf(f, "%"PRId64, expr.integer.value)) return CGEN_IO_ERROR;
+        break;
+    case AST_EXPR_BINARY:
+        if (!(error = rvic_c_generate_c_expr(expr.lhs))) return error;
+        if (!fprintf(f, " %s ", expr.op)) return CGEN_IO_ERROR;
+        if (!(error = rvic_c_generate_c_expr(expr.rhs))) return error;
+        break;
+    }
+    return CGEN_OK;
+}
--- bootstrap/rvic-c_backend.h
+++ bootstrap/rvic-c_backend.h
@@ -1,6 +1,19 @@
 #ifndef RVIC_C_BACKEND
 #define RVIC_C_BACKEND
 
+#include <stdio.h>  // FILE.
 
+#include "ast.h"
+
+enum cgen_error {
+    CGEN_OK,
+    CGEN_GENERIC_ERROR,
+    CGEN_UNEXPECTED_EXPR,
+    CGEN_IO_ERROR,
+};
+
+enum cgen_error rvic_c_generate_c_file(int n, const struct ast_node nodes[const n], FILE *f);
+enum cgen_error rvic_c_generate_c_stmt(struct ast_stmt stmt, int indent, int indent_step, FILE *f);
+enum cgen_error rvic_c_generate_e_expr(struct ast_expr expr, FILE *f);
 
 #endif
