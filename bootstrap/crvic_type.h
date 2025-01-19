57749d06d09933404d6dc50b0995faa4c9c2dcb7
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2025-01-19 00:33:58 +0000

    Change names of types

--- bootstrap/crvic_type.h
+++ bootstrap/crvic_type.h
@@ -5,14 +5,16 @@ enum type_primitive {
     TYPE_NO_TYPE,      // Sentinel type denoting no type.
     TYPE_ABSURD,       // Absurd type `!`. Used to denote expressions that do not return.
     TYPE_UNIT,         // Unit type `()`. Used to denote an expression with no meaningful value.
+    TYPE_I8,           // Signed 8-bit integer.
+    TYPE_I16,          // Signed 16-bit integer.
+    TYPE_I32,          // Signed 32-bit integer.
+    TYPE_I64,          // Signed 64-bit integer.
     TYPE_U8,           // Unsigned 8-bit integer.
     TYPE_U16,          // Unsigned 16-bit integer.
     TYPE_U32,          // Unsigned 32-bit integer.
     TYPE_U64,          // Unsigned 64-bit integer.
-    TYPE_S8,           // Signed 8-bit integer.
-    TYPE_S16,          // Signed 16-bit integer.
-    TYPE_S32,          // Signed 32-bit integer.
-    TYPE_S64,          // Signed 64-bit integer.
+    TYPE_INT,          // Signed pointer-sized integer.
+    TYPE_UINT,         // Unsigned pointer-sized integer.
 };
 
 typedef int TypeID;   // Type for types.
