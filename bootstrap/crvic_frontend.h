4ee13c2d64341a626d46bc676940d9b67e9a6275
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2025-01-27 02:56:14 +0000

    Add all (most?) tokens to lexer

--- bootstrap/crvic_frontend.h
+++ bootstrap/crvic_frontend.h
@@ -1,11 +1,12 @@
 #ifndef CRVIC_FRONTEND_H
 #define CRVIC_FRONTEND_H
 
+#include <stdio.h>
 #include "lexel.h"  // struct lxl_token, LXL_TOKENS_END, LXL_LINE_ENDING.
 
+#include "region.h"
+
 enum token_type {
-    // Identifier.
-    TOKEN_IDENTIFIER,
     // Literals.
     TOKEN_LIT_FLOAT,
     TOKEN_LIT_INTEGER,
@@ -18,31 +19,84 @@ enum token_type {
     TOKEN_BKT_SQUARE_LEFT,
     TOKEN_BKT_SQUARE_RIGHT,
     // Other Punctuation.
+    TOKEN_AMPERSAND,
+    TOKEN_AMPERSAND_EQUALS,
     TOKEN_AT,
+    TOKEN_BANG,
+    TOKEN_BANG_EQUALS,
+    TOKEN_CARET,
+    TOKEN_COLON,
+    TOKEN_COLON_EQUALS,
     TOKEN_COMMA,
+    TOKEN_DOLLAR,
     TOKEN_DOT,
+    TOKEN_EQUALS,
+    TOKEN_EQUALS_EQUALS,
     TOKEN_MINUS,
+    TOKEN_MINUS_EQUALS,
     TOKEN_PERCENT,
+    TOKEN_PERCENT_EQUALS,
     TOKEN_PLUS,
+    TOKEN_PLUS_EQUALS,
+    TOKEN_QMARK,
     TOKEN_SEMICOLON,
     TOKEN_SLASH,
+    TOKEN_SLASH_EQUALS,
     TOKEN_STAR,
+    TOKEN_STAR_EQUALS,
+    TOKEN_TILDE,
+    TOKEN_TILDE_EQUALS,
+    TOKEN_VBAR,
+    TOKEN_VBAR_EQUALS,
+    // Identifier.
+    TOKEN_IDENTIFIER,
+    TOKEN_UNDERSCORE,
     // Keywords.
     TOKEN_KW_AND,
     TOKEN_KW_AS,
+    TOKEN_KW_COMPLEX32_32,
+    TOKEN_KW_COMPLEX64_64,
+    TOKEN_KW_ELSE,
     TOKEN_KW_ENUM,
     TOKEN_KW_EXTERNAL,
+    TOKEN_KW_F32,
+    TOKEN_KW_F64,
+    TOKEN_KW_FOR,
     TOKEN_KW_FUNC,
+    TOKEN_KW_I8,
+    TOKEN_KW_I16,
+    TOKEN_KW_I32,
+    TOKEN_KW_I64,
+    TOKEN_KW_IF,
+    TOKEN_KW_INT,
+    TOKEN_KW_LOOP,
+    TOKEN_KW_MUT,
+    TOKEN_KW_OUT,
     TOKEN_KW_OR,
     TOKEN_KW_RECORD,
     TOKEN_KW_RETURN,
+    TOKEN_KW_THEN,
     TOKEN_KW_TO,
+    TOKEN_KW_U8,
+    TOKEN_KW_U16,
+    TOKEN_KW_U32,
+    TOKEN_KW_U64,
+    TOKEN_KW_UINT,
+    TOKEN_KW_UNINIT,
+    TOKEN_KW_WHEN,
+    TOKEN_KW_WHILE,
 };
 
+// Lexer.
 void init_lexer(const char *source, size_t length);
 struct lxl_token next_token(void);
 
-void print_token(struct lxl_token token);
-void print_tokens(void);
+const char *token_type_string(struct lxl_token token);
+void print_token(struct lxl_token token, FILE *f);
+void print_tokens(FILE *f);
+
+// Parser.
+struct ast_list parse(struct region *region);   // NOTE: call `init_lexer()` first!
+
 
 #endif
