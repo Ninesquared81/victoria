5aa5f6fa2f97029a97f0694ba7465420e33d9db8
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2025-02-03 21:54:36 +0000

    Add Emacs major mode for Victoria
    For now, we're piggybacking off go-mode since the syntax is similar
    enough to Victoria. We'll properly roll our own later, but this will
    do for now.

---
+++ editor/victoria-mode.el
@@ -0,0 +1,20 @@
+;;; victoria-mode.el -- Major mode for Victoria -*- lexical-bingind: t -*-
+
+(require 'go-mode)
+
+(defconst victoria-mode-syntax-table
+  (with-syntax-table (copy-syntax-table)
+    ;; Comments.
+    (modify-syntax-entry ?# "<")
+    (modify-syntax-entry ?\n ">")
+    (syntax-table))
+  "Syntax table for victoria-mode")
+
+(define-derived-mode victoria-mode go-mode "vic"
+  "Major mode for Victoria"
+  (setq comment-start "#")
+  (setq indent-tabs-mode nil)
+  (setq tab-width 4)
+  :syntax-table 'victoria-mode-syntax-table)
+
+(provide 'victoria-mode)
