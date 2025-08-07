5b35fed567bd38e17cc30f4feb0a5a1879743e3c
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2025-08-07 16:40:37 +0100

    Improve syntax highlighting
    We're still relying on `go-mode` for indentation because that is a
    pain to set up.

--- editor/victoria-mode.el
+++ editor/victoria-mode.el
@@ -10,12 +10,45 @@
     (syntax-table))
   "Syntax table for victoria-mode")
 
+(defconst victoria-keywords
+  '("and" "as" "const" "else" "enum" "external"
+    "for" "func" "if" "loop" "mut" "not" "or" "out"
+    "record" "return" "then" "to" "type" "val" "var"
+    "when" "while")
+  "Victoria keywords (except types and constants)")
+
+(defconst victoria-types
+  '("bool" "c_string" "complex32_32" "complex64_64"
+    "f32" "f64" "i8" "i16" "i32" "i64" "int"
+    "string" "u8" "u16" "u32" "u64" "uint")
+  "Built-in Victoria data types")
+
+(defconst victoria-builtins
+  '("count_of" "type_of" "size_of")
+  "Built-in (magic) Victoria functions")
+
+(defconst victoria-constants
+  '("false" "null" "true" "uninit")
+  "Victoria constants")
+
+(defconst victoria-string-prefix-regexp
+  '("\\<c\"")
+  "Regexps for string prefixes in Victoria")
+
+(defconst victoria-highlighting
+  `((,(regexp-opt victoria-builtins 'symbols)  . font-lock-builtin-face)
+    (,(regexp-opt victoria-keywords 'symbols)  . font-lock-keyword-face)
+    (,(regexp-opt victoria-types 'symbols)     . font-lock-type-face)
+    (,(regexp-opt victoria-constants 'symbols) . font-lock-constant-face))
+    "Syntax highlighting for victoria-mode")
+
 ;;;###autoload
 (define-derived-mode victoria-mode go-mode "vic"
   "Major mode for Victoria"
   (setq comment-start "#")
   (setq indent-tabs-mode nil)
   (setq tab-width 4)
+  (setq font-lock-defaults '(victoria-highlighting))
   :syntax-table 'victoria-mode-syntax-table)
 
 ;;;###autoload
